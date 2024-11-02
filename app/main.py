42670f72262a17d4269cd6f0f35a2702e1cd487b
cauesmelo <cauesmelo@gmail.com>
2024-11-02 19:54:04 -0300

    add test for ping

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from tortoise import Tortoise
 
 from app.settings import Settings, get_settings
 from tortoise.contrib.fastapi import register_tortoise
@@ -7,14 +8,15 @@ app = FastAPI()
 
 settings = get_settings()
 
-register_tortoise(
-    app,
-    db_url=settings.database_url,
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=False,
-    add_exception_handlers=True,
-)
+async def init():
+    await Tortoise.init(
+        db_url=settings.database_url,
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
 
+    await Tortoise.generate_schemas()
 
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
---
+++ tests/test_ping.py
@@ -0,0 +1,17 @@
+from app import main
+
+def test_ping(test_app):
+    # Given
+    # test_app
+
+    # When
+    response = test_app.get("/ping")
+
+    # Then
+    assert response.status_code == 200
+    assert response.json() == {
+            "environment": "DEV", 
+            "ping": "pong!", 
+            "testing": True,
+            'database_url': 'postgres://postgres:postgres@db:5432/web_test'
+        }
