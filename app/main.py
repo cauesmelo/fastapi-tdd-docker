3d4fafef9106774a455678c9628b1c97ff422303
cauesmelo <cauesmelo@gmail.com>
2024-11-03 15:09:13 -0300

    fix lifespan deprecation

--- app/main.py
+++ app/main.py
@@ -1,31 +1,31 @@
+from contextlib import asynccontextmanager
 import logging
-
 from fastapi import FastAPI
-
 from app.api import ping, summaries
 from app.db import init_db
+from app.settings import get_settings
+
+settings = get_settings()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    log.info("Starting up...")
+
+    await init_db()
+
+    log.info("Database connected.")
+    yield
+
+    log.info("Stoppping...")
 
 
 log = logging.getLogger("uvicorn")
 
 
-def create_application() -> FastAPI:
-    application = FastAPI()
+def create_application(*, lifespan = None) -> FastAPI:
+    application = FastAPI(lifespan=lifespan)
     application.include_router(ping.router)
     application.include_router(summaries.router)
-
     return application
 
-
-app = create_application()
-
-
-@app.on_event("startup")
-async def startup_event():
-    log.info("Starting up...")
-    init_db(app)
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    log.info("Shutting down...")
+app = create_application(lifespan=lifespan)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,29 @@
+from contextlib import asynccontextmanager
 import os
-
+from fastapi import FastAPI
 import pytest
 from starlette.testclient import TestClient
 from tortoise.contrib.fastapi import register_tortoise
+from app.db import clear_test_db, init_test_db
 from app.main import create_application
 from app.settings import get_settings, Settings
+from tortoise import Tortoise
+from tortoise import Tortoise, generate_config
+from tortoise.contrib.fastapi import RegisterTortoise
 
 def get_settings_override():
     return Settings(
         testing=1,
         database_url=os.environ.get("DATABASE_TEST_URL")
     )
+
+@asynccontextmanager
+async def test_lifespan(app: FastAPI):
+    await init_test_db()
+
+    yield
+
+    await clear_test_db()
 
 @pytest.fixture(scope="module")
 def test_app():
@@ -26,16 +39,8 @@ def test_app():
 
 @pytest.fixture(scope="module")
 def test_app_with_db():
-    app = create_application()
+    app = create_application(lifespan=test_lifespan)
     app.dependency_overrides[get_settings] = get_settings_override
-
-    register_tortoise(
-        app,
-        db_url=os.environ.get("DATABASE_TEST_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=True,
-        add_exception_handlers=True,
-    )
 
     with TestClient(app) as test_client:
 
