b6406ea053704fbff6a0cc0fb4a7e58f71d27007
cauesmelo <cauesmelo@gmail.com>
2024-11-03 11:50:23 -0300

    add routes for summaries

--- app/api/summaries.py
+++ app/api/summaries.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Response
+from typing import List
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
-
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
 from app.api import crud
+from app.models.tortoise import SummarySchema
 
 
 router = APIRouter(prefix="/summaries")
@@ -20,3 +21,19 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
         status_code=201,
         content=response_object
     )
+
+@router.get("/{id}")
+async def read_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+
+    if summary is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Summary not found"
+        )
+
+    return summary
+
+@router.get("/")
+async def read_all_summaries() -> List[SummarySchema]:
+    return await crud.get_all()
--- tests/test_summaries.py
+++ tests/test_summaries.py
@@ -16,3 +16,37 @@ def test_create_summaries_invalid_json(test_app_with_db):
     response = test_app_with_db.post("/summaries/", data=json.dumps(payload))
 
     assert response.status_code == 422
+
+def test_read_summary(test_app_with_db):
+    # create new
+    payload = {"url": "https://foo.bar"}
+    response = test_app_with_db.post("/summaries/", data=json.dumps(payload))
+    id = response.json()["id"]
+
+    # get created
+    response = test_app_with_db.get(f"/summaries/{id}/")
+    response_dict = response.json()
+
+    assert response.status_code == 200
+    assert response_dict["id"] == id
+    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["summary"]
+    assert response_dict["created_at"]
+
+def test_read_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.get(f"/summaries/999")
+
+    assert response.status_code == 404
+
+def test_read_all_summaries(test_app_with_db):
+    payload = {"url": "https://foo.bar"}
+    response = test_app_with_db.post("/summaries", data=json.dumps(payload))
+
+    id = response.json()["id"]
+
+    response = test_app_with_db.get("/summaries/")
+    assert response.status_code == 200
+
+    response_list = response.json()
+    filter_by_id = list(filter(lambda d: d["id"] == id, response_list))
+    assert len(filter_by_id) == 1
