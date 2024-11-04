22b9d2c65edbec3213829e0fdb53f3034c9c8e0e
cauesmelo <cauesmelo@gmail.com>
2024-11-03 21:10:49 -0300

    fix bad test

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -7,9 +7,3 @@ def test_ping(test_app):
 
     # Then
     assert response.status_code == 200
-    assert response.json() == {
-        "environment": "DEV",
-        "ping": "pong!",
-        "testing": True,
-        "database_url": "postgres://postgres:postgres@db:5432/web_test",
-    }
